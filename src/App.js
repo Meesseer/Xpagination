84fbb82f39d216032d96ba9a516acc2aa5da8ac1
Ammar Khan <ammarasu@gmail.com>
2024-10-20 14:45:00 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage]= useState(10)
+
+  const fetchData = async() =>{
+    try{
+      const response = await fetch(`https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json`)
+      const result = await response.json();
+      console.log(result)
+      setData(result)
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstitem = indexOfLastItem - itemsPerPage;
+  const currentData = data.slice(indexOfFirstitem,indexOfLastItem); 
+
+  const totalPages = Math.ceil(data.length/itemsPerPage);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <table>
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentData.map((member, index)=>(
+            <tr key={member.id}>
+              {/* <td>{index + 1}</td> */}
+              <td>{member.id}</td>
+              <td>{member.name}</td>
+              <td>{member.email}</td>
+              <td>{member.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => paginate(i + 1)}
+            className={currentPage === i + 1 ? 'active' : ''}
+          >
+            {i + 1}
+          </button>
+        ))}
+
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
